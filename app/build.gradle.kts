cac0cb4f48f6fea6c8563948491f758543388444
drwpls <phanlocson@gmail.com>
2026-06-23 07:25:07 +0000

    fix: sign with apksigner directly in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,15 +32,6 @@ android {
 
     buildTypes {
         release {
-            val keyPath = System.getenv("KEYSTORE_PATH")
-            if (keyPath != null) {
-                signingConfig = signingConfigs.create("release") {
-                    storeFile = file(keyPath)
-                    storePassword = System.getenv("KEYSTORE_PASSWORD")
-                    keyAlias = System.getenv("KEY_ALIAS")
-                    keyPassword = System.getenv("KEY_PASSWORD")
-                }
-            }
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
