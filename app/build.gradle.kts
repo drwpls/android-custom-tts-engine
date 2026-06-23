369329b8c63f47bb481e6e1eb89f4cdac94f611f
drwpls <phanlocson@gmail.com>
2026-06-23 07:04:06 +0000

    fix: use System.getenv with non-null assertion for signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,13 +32,10 @@ android {
 
     signingConfigs {
         create("release") {
-            val keystorePath = System.getenv("KEYSTORE_PATH")
-            if (keystorePath != null) {
-                storeFile = file(keystorePath)
-                storePassword = System.getenv("KEYSTORE_PASSWORD")
-                keyAlias = System.getenv("KEY_ALIAS")
-                keyPassword = System.getenv("KEY_PASSWORD")
-            }
+            storeFile = file(System.getenv("KEYSTORE_PATH")!!)
+            storePassword = System.getenv("KEYSTORE_PASSWORD")!!
+            keyAlias = System.getenv("KEY_ALIAS")!!
+            keyPassword = System.getenv("KEY_PASSWORD")!!
         }
     }
 
